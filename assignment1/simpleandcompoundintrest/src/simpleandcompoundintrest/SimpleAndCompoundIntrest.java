9d81a1835f520d42ea42a648ee4c9ee121dd1050
srinadhuSrikanya <93194424+srinadhuSrikanya@users.noreply.github.com>
2021-12-23 09:46:54 +0530

    committed

---
+++ assignment1/simpleandcompoundintrest/src/simpleandcompoundintrest/SimpleAndCompoundIntrest.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+import java.util.*;
+
+import java.lang.Math;
+
+public class SimpleAndCompoundIntrest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
